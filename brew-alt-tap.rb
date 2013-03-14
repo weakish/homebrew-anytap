65742f74296367e7c79700db407d3a3a5d6194bd
Peter Aronoff <telemachus@arpinum.org>
2013-03-14 11:39:36 -0400

    I make that typo too often

--- brew-alt-tap.rb
+++ brew-alt-tap.rb
@@ -12,9 +12,9 @@ EOF
 
 if ARGV.size < 1 or ['-h', '-?', '--help'].include?(ARGV.first)
   puts usage
-elsif ARGV.include('--raw') or ARGV.include('-r')
+elsif ARGV.include?('--raw') or ARGV.include?('-r')
   # Use exactly what the user gives us
-elsif ARGV.include('--private') or ARGV.include('-p')
+elsif ARGV.include?('--private') or ARGV.include?('-p')
   # A private repo means we shouldn't check for existence via curl
 else
   puts "Proof of Concept"
