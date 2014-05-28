72ea364cdee08d6120ee86f5678c0dc06a68426f
Peter Aronoff <telemachus@arpinum.org>
2014-05-28 10:23:34 -0400

    Bump version to 0.0.8

--- Formula/brew-any-tap.rb
+++ Formula/brew-any-tap.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewAnyTap < Formula
   homepage 'https://github.com/telemachus/homebrew-anytap/'
   url 'https://github.com/telemachus/homebrew-anytap.git'
-  version '0.0.7'
+  version '0.0.8'
 
   skip_clean 'bin'
 
